0868cd7d63ebcfa37f1382bb5a3db362dbbb5227
Anastasia Salikova <fotograf.anastasia@gmail.com>
2019-06-19 22:26:49 +0300

    test to start with pytest

---
+++ test_section_3/test_3_3_1_pytest.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import unittest
+import pytest
+
+
+class TestSection(unittest.TestCase):
+
+    def setUp(self):
+        self.wd = webdriver.Chrome("C:\Tools\Selenium\chromedriver.exe")
+
+    def register_user(self, url):
+        wd = self.wd
+        wd.get(url)
+        name = wd.find_element_by_css_selector(".first_block .first_class .first")
+        name.clear()
+        name.send_keys("Anastasia")
+        last_name = wd.find_element_by_css_selector(".first_block .second_class .second")
+        last_name.clear()
+        last_name.send_keys("S")
+        email = wd.find_element_by_css_selector(".first_block .third_class .third")
+        email.clear()
+        email.send_keys("randomnanastya@gmail.com")
+
+        button = wd.find_element_by_css_selector("button.btn")
+        button.click()
+
+        welcome_text_elt = wd.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+
+        return welcome_text
+
+    def test_register(self):
+        res = self.register_user("http://suninjuly.github.io/registration1.html")
+        mes = "Поздравляем! Вы успешно зарегистировались!"
+
+        assert res == mes, "failed to register"
+
+    def test_error_register(self):
+        res = self.register_user("http://suninjuly.github.io/registration2.html")
+        mes = "Поздравляем! Вы успешно зарегистировались!"
+
+        assert res == mes, "failed to register"
+
+
+if __name__ == "__main__":
+    unittest.main()
