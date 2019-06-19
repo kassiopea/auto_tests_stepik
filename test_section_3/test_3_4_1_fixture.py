646db3f95650d4e123d2578c14ad8a568b19efe3
Anastasia Salikova <fotograf.anastasia@gmail.com>
2019-06-19 22:28:55 +0300

    add fixture to the test

---
+++ test_section_3/test_3_4_1_fixture.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
+
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        pass
+        # какие-то проверки
+
+    def test_second_smiling_faces(self, prepare_faces):
+        pass
+        # какие-то проверки
